d9673fd3e356b56c94c63f3bd5c42c8413b14d8f
Samuel Carvalho <samuelcarv04@gmail.com>
2024-08-27 08:18:24 -0300

    problem 03

--- week03/exercicies/runoff.c
+++ week03/exercicies/runoff.c
@@ -4,10 +4,20 @@
 #define MAX_VOTERS 100
 #define MAX_CANDIDATES 9
 
+bool vote(char *voter, int rank, char *name);
+
 typedef struct {
   char *name;
   int votes;
   bool eliminated;
 } candidate;
 
-int main(void) { candidate candidates[MAX_CANDIDATES]; }
+int voter_count;
+int candidate_count;
+
+int main(void) {
+  candidate candidates[MAX_CANDIDATES];
+  int preferences[MAX_VOTERS][MAX_CANDIDATES];
+}
+
+bool vote(char *voter, int rank, char *name) { return true; }
